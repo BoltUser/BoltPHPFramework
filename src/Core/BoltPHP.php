3b72e97018f6a7599aec7d7f225cf22e82587e20
BoltUser <boltphp@gmail.com>
2017-12-24 14:03:08 +0000

    Composer update

--- src/Core/BoltPHP.php
+++ src/Core/BoltPHP.php
@@ -11,9 +11,7 @@ class BoltPHP
 
     public function __construct()
     {
-        $whoops = new \Whoops\Run;
-        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-        $whoops->register();
+
     }
 
     public function run()
