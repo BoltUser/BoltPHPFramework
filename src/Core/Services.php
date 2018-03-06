20d86027d03a2eec0811ecac5a4400bb1d8ed757
BoltPHP Git <boltphp@gmail.com>
2018-03-06 15:47:36 +0000

    endWith Modified

--- src/Core/Services.php
+++ src/Core/Services.php
@@ -12,8 +12,19 @@ class Services
     public function load()
     {
         $stack = array_reverse(debug_backtrace());
-        $app_path = dirname(dirname($stack[0]['file']));
-        $servicePath = $app_path . "/services/";
+        $app_path = dirname($stack[0]['file']);
+        $found = FALSE;
+        while (BASE_PATH != $app_path) {
+            if (is_dir($servicePath = $app_path . "/services/")) {
+                $found = TRUE;
+                break;
+            }
+            $app_path = dirname($app_path);
+        }
+        if ($found === FALSE) {
+            throw new \Exception('Services Folder Not Found!');
+        }
+
         foreach (glob($servicePath . '*.php') as $file) {
             if (file_exists($file))
                 require_once($file);
