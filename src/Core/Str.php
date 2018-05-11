1f825384bf26439fbffb43bb591fdbfb3b37ab6b
BoltPHP Git <boltphp@gmail.com>
2018-05-11 15:05:20 +0100

    endWith Modified

--- src/Core/Str.php
+++ src/Core/Str.php
@@ -55,6 +55,14 @@ class Str
         return $result;
     }
 
+    public static function convertToReadableSize($size)
+    {
+        $base = log($size) / log(1024);
+        $suffix = array("", "KB", "MB", "GB", "TB");
+        $f_base = floor($base);
+        return round(pow(1024, $base - floor($base)), 1) . $suffix[$f_base];
+    }
+
     /**
      * Make string safe
      * - Remove UTF-8 chars
